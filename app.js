6805f484a378594684a665a305cec9566e1e4c21
Ophelia Yin <owyowy1@gmail.com>
2016-12-02 13:57:20 -0800

    modified problem 2

--- app.js
+++ app.js
@@ -16,25 +16,28 @@ function sum(a,b){ //eslint-disable-line
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+//testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
-/* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-
-"The product of 5 and 9 is 45."
-
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
+ //Problem 2
+// Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+//
+// "The product of 5 and 9 is 45."
+//
+// Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.
 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
-
+  var result = a * b;
+  var message = 'The product of ' + a + ' and ' + b
+  + ' is ' + result + '.';
+  return [result, message];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
